5fb55ad9665d40fdf3176ca333bec5c6956ba498
robert-walsh-hrri <rwalsh@gmail.com>
2020-08-19 18:17:43 -0500

    Final fit and finish, moving to deployment

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(bodyparser.json());
 app.use('/api/images/:product_id', createProxyMiddleware({ target: 'http://localhost:5000/', changeOrigin: true}));
 app.use('/api/midpageimages/:id', createProxyMiddleware({ target: 'http://localhost:7000/', changeOrigin: true}));
 app.use('/api/productoptions/:id', createProxyMiddleware({ target: 'http://localhost:3001/', changeOrigin: true }));
-app.use('/api/productreviews/reviews', createProxyMiddleware({ target: 'http://localhost:4000/', changeOrigin: true}));
+app.use('/api/productreviews/:reviewId', createProxyMiddleware({ target: 'http://localhost:4000/', changeOrigin: true}));
 
 app.get('/:id', (req, res) => {
   res.sendFile('index.html', {root: path.join(__dirname, '../public')});
