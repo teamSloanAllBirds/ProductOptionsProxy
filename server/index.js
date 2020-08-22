4fd7a8e412b8f16269408833fa1ca8f84d8845b5
EC2 Default User <ec2-user@ip-172-31-22-121.us-east-2.compute.internal>
2020-08-22 17:32:18 +0000

    Server side changes

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const jquery = require('jquery');
 const path = require('path');
 const bodyparser = require('body-parser');
+const compression = require('compression');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const port = 3000;
 
+app.use(compression());
 app.use(express.static(path.resolve(__dirname, '../public/')));
 app.use(bodyparser.urlencoded({
   extended: true,
